005ecdb9a7b51189b968cfec0ed6603b54943a48
Bence Varga <vargab2@starschema.net>
2022-11-08 16:24:58 +0100

    Moved functions to Actions_enum.py file

--- Actions_enum.py
+++ Actions_enum.py
@@ -11,3 +11,25 @@ class Actions(Enum):
     list = 8
     sell = 9
     quit = 10
+
+# Print a menu with the avaliable options
+# Dynamic numbers, which come from the Actions class in Actions_enum.py file
+def print_menu():
+    print(10 * '-' + 'MENU' + 10 * '-')
+    print(f'Press {Actions.create.value} to create')  
+    print(f'Press {Actions.sick.value} to log sickness')  
+    print(f'Press {Actions.operation.value} to log operation')  
+    print(f'Press {Actions.heal.value} to log healing')  
+    print(f'Press {Actions.vaccine.value} to log vaccination')  
+    print(f'Press {Actions.inquire.value} to get animal data')  
+    print(f'Press {Actions.history.value} to get the history of an animal')  
+    print(f'Press {Actions.list.value} to list animal names and IDs')  
+    print(f'Press {Actions.sell.value} to sell') 
+    print(f'Press {Actions.quit.value} to quit')  
+    print(24 * '-')
+
+# Require users to press enter before returning to main menu
+def enter_to_continue():
+    continue_to_next = input('Press enter to continue to main menu!')
+    if continue_to_next is True:
+        pass
--- Animal_shop.py
+++ Animal_shop.py
@@ -2,32 +2,10 @@ from datetime import datetime
 from animal_class import Animal
 import time
 from Actions_enum import Actions
+from Actions_enum import print_menu
+from Actions_enum import enter_to_continue
 
 animals_dict = {}
-
-# Print a menu with the avaliable options
-# Dynamic numbers, which come from the Actions class in Actions_enum.py file
-def print_menu():
-    print(10 * '-' + 'MENU' + 10 * '-')
-    print(f'Press {Actions.create.value} to create')  
-    print(f'Press {Actions.sick.value} to log sickness')  
-    print(f'Press {Actions.operation.value} to log operation')  
-    print(f'Press {Actions.heal.value} to log healing')  
-    print(f'Press {Actions.vaccine.value} to log vaccination')  
-    print(f'Press {Actions.inquire.value} to get animal data')  
-    print(f'Press {Actions.history.value} to get the history of an animal')  
-    print(f'Press {Actions.list.value} to list animal names and IDs')  
-    print(f'Press {Actions.sell.value} to sell') 
-    print(f'Press {Actions.quit.value} to quit')  
-    print(24 * '-')
-
-
-# Require users to press enter before returning to main menu
-def enter_to_continue():
-    continue_to_next = input('Press enter to continue to main menu!')
-    if continue_to_next is True:
-        pass
-
 
 while True:
     print_menu()
@@ -151,7 +129,7 @@ while True:
         print(f'Created an animal with the ID: {animal_temp.get_id()}')
 
         enter_to_continue()
-        
+
     # Getting the data of the animal
     elif event_code == Actions.inquire.value:
         animal_id = int(input("Please enter the ID of the animal: "))
